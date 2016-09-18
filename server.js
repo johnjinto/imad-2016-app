e5130e06a45c797d556cbfbf7280c8a8bfaa3adc
johnjinto <johnjinto2012@gmail.com>
2016-09-18 20:43:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Article one');
+  res.send('This is nice');
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('Article two');
+  res.send('This is nicer');
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article three');
+  res.send('Even more nice');
 });
 
 app.get('/ui/style.css', function (req, res) {
