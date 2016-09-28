218c6052e755937db0397ec18306453268d5370d
johnjinto <johnjinto@users.noreply.github.com>
2016-09-29 00:39:32 +0530

    client side javascript added

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,21 @@
 console.log('Loaded!');
+
+var element = document.getElementById('phrase');
+
+element.innerHTML = 'Jesus is God';
+
+var img = document.getElementById('image1');
+
+var marginLeft = 0;
+function moveRight(){ 
+
+marginLeft = marginLeft+10;
+image1.style.marginLeft = marginLeft + 'px';
+
+};
+
+img.onclick = function (){
+
+var interval = setInterval(moveRight, 100); 
+
+};
