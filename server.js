9582969cc3c2e0bbf866143374db5c572f16cd2e
johnjinto <johnjinto@users.noreply.github.com>
2016-11-28 02:52:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,10 +97,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-//app.get('/:articleName', function (req, res) {
-app.get('/1', function (req, res) {
+app.get('/:articleName', function (req, res) {
+//app.get('/1', function (req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articleone));
+  res.send(console.log(articleName));
+  
 });
 
 
