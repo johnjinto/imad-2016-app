e12d0c5e9386602ee3fd18e7330ba121e9549c8c
johnjinto <johnjinto@users.noreply.github.com>
2016-11-28 03:38:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ function createTemplate (data){
 </html>
 
 `;
-  console.log('consolelogsuccess');
+
     
     return htmlTemplate;
 }
@@ -126,4 +126,5 @@ app.get('/ui/madi.png', function (req, res) {
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
+    console.log('consolelogsuccess');
 });
