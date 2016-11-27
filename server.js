c676d6b90d54d8aef13ef5ab7538c02a27de2feb
johnjinto <johnjinto@users.noreply.github.com>
2016-11-28 03:34:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,7 @@ function createTemplate (data){
 </html>
 
 `;
+  console.log('consolelogsuccess');
     
     return htmlTemplate;
 }
@@ -104,8 +105,6 @@ app.get('/:articleName', function (req, res) {
 //app.get('/1', function (req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articleone));
-  console.log('consolelogsuccess');
-  
 });
 
 
@@ -116,7 +115,7 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-  console.log('consolelogsuccess');
+
 });
 
 app.get('/ui/madi.png', function (req, res) {
